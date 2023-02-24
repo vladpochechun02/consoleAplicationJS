f279d5329fcb2c732a8dc0200facac9f87de060b
Vladyslav <kpigoodvlad@gmail.com>
2023-02-24 13:09:22 +0200

    Created basic project for working on a console aplication

---
+++ js.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+// console.log(incr +' ' + decr);
+
+// alert('Who are you?');
+
+// const result = confirm('Are you here?');
+
+// const result = prompt('Hoq old are you?', '18');
+// console.log('type: ' + typeof(result) + '\nvalue: ' + result);
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', 'Someone');
+// answers[1] = prompt('Hoq old are you?', '18');
+// answers[2] = prompt('Your female?', 'Man');
+
+// console.log(answers);
+// document.write(answers);
+
+// const name = prompt('What is your name?', 'Name');
+
+// alert(`Hello ${name} :)`);
